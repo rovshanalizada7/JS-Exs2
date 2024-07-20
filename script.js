80ccaf2bb89e2c56c3523f92d6e698d78a1a59f2
Rovshan Alizada <rovshanalizada@gamil.com>
2024-07-20 12:18:10 +0400

    exs done

---
+++ script.js
@@ -0,0 +1,138 @@
+// 1) for ile quracaginiz task
+// *
+// **
+// ***
+// ****
+// *****
+
+
+// for (let i=1; i<=5; i++) {
+//   let star= "";
+//  for(let j=1;j<=i;j++){
+//     star+="*"
+//  }
+//   console.log(star);
+// }
+
+
+
+//-------------------------------------------------------------
+
+
+
+// 2) for ile quracaginiz task
+// *****
+// ****
+// ***
+// **
+// *
+
+// for (let i=5; i>=1; i--){
+//  let star = '' ;
+//  for (let j=1; j<=i;j++) {
+//     star += "*" ;
+//  }
+//  console.log(star);
+// }
+
+
+
+
+// --------------------------------------------------------------------
+
+
+//3) toplayan bolen vuran  cixan parametrli  js funksiyalarin yaradin
+// her birini ferqli sekilde yazmisam eyni olmasin deye
+
+
+// function sum  (a,b)  {
+//     console.log(a+b);
+// }
+// sum(4,5)
+
+
+
+// const subtraction = (a,b) => {
+//     return a-b;
+// }
+
+// let result = subtraction(4,5)
+// console.log(result);
+
+
+
+// const multiply = (a,b) => {
+//     let result = a*b;
+//     return result;
+// }
+
+// console.log(multiply(3,5));
+
+
+
+// function division (a,b)  {
+//     console.log(a/b); ;
+// }
+
+// division (15,5)
+
+
+//----------------------------------------------------------------------------
+
+
+//4) Ədədin faktorialını hesablayan funksiyanı yazın. 
+// Funksiya n ədədini götürməli və n deməlidir! Qaytarmalıdır (n faktorial)
+//  For və ya while loopundan istifadə edin.
+
+
+// const factorial= (n) =>{
+//     if (n < 0) return undefined;
+//     let result = 1;
+//     for (let i = 1; i <= n; i++) {
+//         result *= i;
+//     }
+//     return result;
+// }
+
+// console.log(factorial (4));
+
+
+//--------------------------------------------------------------------------------
+
+
+// 5) let newArray= [10,1,2,3,8,9,99,77,44,5];
+// Massivdə ən böyük dəyəri tapan funksiyanı yazın. 
+// Funksiya ədədlər massivi götürməli və massivdəki ən böyük ədədi qaytarmalıdır.
+//  Bunu for və ya while loopundan istifadə edərək həll edin.
+
+
+// let newArray = [10,1,2,3,8,9,99,77,44,5];
+// let largest = 0 ;
+
+// for ( let i=0; i<newArray.length; i++ ) {
+//     if(newArray[i]>largest) {
+//         largest = newArray[i]
+//     }
+// }
+
+// console.log(largest);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
